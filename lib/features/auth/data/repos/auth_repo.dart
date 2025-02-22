dba95c5a757858f0dd8170c97bcfff399c8b9aa7
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2025-02-22 02:27:29 +0200

    - added verify email impl in repo

--- lib/features/auth/data/repos/auth_repo.dart
+++ lib/features/auth/data/repos/auth_repo.dart
@@ -4,6 +4,7 @@ import 'package:chitchat/core/errors/failure.dart';
 import 'package:chitchat/features/auth/data/datasource/auth_datasource.dart';
 import 'package:chitchat/features/auth/data/models/register_request_body.dart';
 import 'package:chitchat/features/auth/data/models/message_response.dart';
+import 'package:chitchat/features/auth/data/models/verify_email_request_body.dart';
 import 'package:dartz/dartz.dart';
 
 class AuthRepo {
@@ -16,6 +17,20 @@ class AuthRepo {
       RegisterRequestBody registerRequestBody) async {
     try {
       final response = await _authDatasource.register(registerRequestBody);
+      return Right(response);
+    } on NetworkException catch (e) {
+      return Left(NetworkFailure(message: e.message));
+    } catch (e) {
+      return Left(ServerFailure(message: e.toString()));
+    }
+  }
+
+  // Verify Email
+  Future<Either<Failure, MessageResponse>> verifyEmail(
+      VerifyEmailRequestBody verifyEmailRequestBody) async {
+    try {
+      final response =
+          await _authDatasource.verifyEmail(verifyEmailRequestBody);
       return Right(response);
     } on NetworkException catch (e) {
       return Left(NetworkFailure(message: e.message));
