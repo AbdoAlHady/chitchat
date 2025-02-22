690b0e6ad5e800231955ac9197f660bfbfee97a7
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2025-02-22 17:17:02 +0200

    added login method to authDatasource

--- lib/features/auth/data/datasource/auth_datasource.dart
+++ lib/features/auth/data/datasource/auth_datasource.dart
@@ -1,6 +1,8 @@
 import 'package:chitchat/core/errors/exceptions.dart';
 import 'package:chitchat/core/helper/connection_helper.dart';
 import 'package:chitchat/core/network/error_handler.dart';
+import 'package:chitchat/features/auth/data/models/login_request_body.dart';
+import 'package:chitchat/features/auth/data/models/login_response.dart';
 import 'package:chitchat/features/auth/data/models/register_request_body.dart';
 import 'package:chitchat/features/auth/data/models/message_response.dart';
 import 'package:chitchat/features/auth/data/models/resend_email_verification_request_body.dart';
@@ -62,6 +64,23 @@ class AuthDatasource {
       rethrow;
     } catch (error) {
       throw ServerException(message: ErrorHandler.hanlde(error).message!);
+    }
+  }
+
+  // Login
+  Future<LoginResponse> login(LoginRequestBody loginRequestBody) async {
+    try {
+      final isConnected = await ConnectionHelper().checkInternetConnection();
+      if (!isConnected) {
+        throw NetworkException(
+            message: 'No internet connection found, please try again later');
+      }
+      final response = await _apiService.login(loginRequestBody);
+      return response;
+    } on NetworkException {
+      rethrow;
+    } catch (e) {
+      throw ServerException(message: ErrorHandler.hanlde(e).message!);
     }
   }
 }
