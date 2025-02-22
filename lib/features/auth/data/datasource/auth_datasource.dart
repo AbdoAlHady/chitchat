c72a48b684f48f4f66210b0b089f30eb94ecec6e
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2025-02-22 02:25:32 +0200

    - added verify email method in data source

--- lib/features/auth/data/datasource/auth_datasource.dart
+++ lib/features/auth/data/datasource/auth_datasource.dart
@@ -3,6 +3,7 @@ import 'package:chitchat/core/helper/connection_helper.dart';
 import 'package:chitchat/core/network/error_handler.dart';
 import 'package:chitchat/features/auth/data/models/register_request_body.dart';
 import 'package:chitchat/features/auth/data/models/message_response.dart';
+import 'package:chitchat/features/auth/data/models/verify_email_request_body.dart';
 
 import '../../../../core/network/api_service.dart';
 
@@ -24,7 +25,24 @@ class AuthDatasource {
     } on NetworkException {
       rethrow;
     } catch (error) {
-      throw NetworkException(message: ErrorHandler.hanlde(error).message!);
+      throw ServerException(message: ErrorHandler.hanlde(error).message!);
+    }
+  }
+
+  Future<MessageResponse> verifyEmail(
+      VerifyEmailRequestBody verifyEmailRequestBody) async {
+    try {
+      final isConnected = await ConnectionHelper().checkInternetConnection();
+      if (!isConnected) {
+        throw NetworkException(
+            message: 'No internet connection found, please try again later');
+      }
+      final response = await _apiService.verifyEmail(verifyEmailRequestBody);
+      return response;
+    } on NetworkException {
+      rethrow;
+    } catch (error) {
+      throw ServerException(message: ErrorHandler.hanlde(error).message!);
     }
   }
 }
